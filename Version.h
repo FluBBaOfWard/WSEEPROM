fec7638a18fa3eff4791039af4f122f0ff997102
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2022-02-11 18:58:33 +0100

    Added History and Version files.

---
+++ Version.h
@@ -0,0 +1,9 @@
+//
+//  Version.h
+//  WSEEPROM
+//
+//  Created by Fredrik Ahlström on 2021-11-10.
+//  Copyright © 2021-2022 Fredrik Ahlström. All rights reserved.
+//
+
+#define WSEEPROMVERSION "V0.5.0"
